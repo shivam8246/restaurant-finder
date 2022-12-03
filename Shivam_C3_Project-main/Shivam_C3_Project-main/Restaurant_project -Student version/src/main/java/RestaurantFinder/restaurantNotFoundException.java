a758ccc55a454e74d8d662e17920e7ce1399fb49
shivam <sssharma8246@gmail.com>
2022-12-03 18:27:46 +0530

    restaturant finder

---
+++ Shivam_C3_Project-main/Shivam_C3_Project-main/Restaurant_project -Student version/src/main/java/RestaurantFinder/restaurantNotFoundException.java
@@ -0,0 +1,8 @@
+package RestaurantFinder;
+
+public class restaurantNotFoundException extends Throwable {
+    public restaurantNotFoundException(String restaurantName) {
+
+        super(restaurantName);
+    }
+}
